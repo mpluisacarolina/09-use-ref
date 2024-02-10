6ffc0caf4118573ec5ae1770846d78ec08f7a2fe
mpluisacarolina <mpluisacarolina@gmail.com>
2024-02-10 16:38:00 -0400

    Ejemplo useRef

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
+import { Formulario } from "./components/Formulario";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-    
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Formulario/>
       </header>
     </div>
   );
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
 export const Formulario = () => {
+
+  const nombreInput = useRef("");
+  const telefonoInput = useRef("");
+  const emailInput = useRef("");
+  const miCaja = useRef("");
+
+  const mostrar = e => {
+    e.preventDefault();
+    console.log(nombreInput.current.value);
+    console.log(telefonoInput.current.value);
+    console.log(emailInput.current.value);
+
+    let {current: caja } = miCaja;
+    caja.classList.add("fondoVerde");
+    caja.innerHTML = "Contacto Guardado";
+  }
+
   return (
     <div>
-        
-        
+      <h2>AGENDA TELEFÓNICA</h2>
+
+      <div ref = {miCaja} className="miCaja">
+        <h3> Pruebas con useRef </h3>
+
+      </div>
+
+      <form onSubmit = {mostrar}>
+        <h4>Nombres y Apellidos: <input type="text" ref={nombreInput}/> </h4>
+        <h4>Teléfono: <input type="text" ref={telefonoInput} /> </h4>
+        <h4>Correo Electrónico: <input type="email" ref = {emailInput} /> </h4>
+        <input type="submit" value="Guardar" />
+      </form>
     </div>
   )
 }
